85d73f4976c63c63ad95845ec703ec9c5770438b
Sergei Ovchinnikov <svo393@gmail.com>
2020-05-05 20:46:31 +0300

    wip

---
+++ tests/seedData.ts
@@ -0,0 +1,15 @@
+const items = [
+  {
+    name: 'Acer Aspire Z24-890-UA91 AIO Desktop, 23.8 inches Full HD, 9th Gen Intel Core i5-9400T, 12GB DDR4, 512GB SSD, 802.11ac Wifi, USB 3.1 Type C, Wireless Keyboard and Mouse, Windows 10 Home, Silver',
+    listPrice: '79999',
+    price: '68615',
+    shortDescription: '9th Generation Intel Core i5 9400T Processor (Up to 3.4GHz)\n23.8 inches Full HD (1920 x 1080) widescreen Edge to Edge LED Back lit Display\n12GB DDR4 Memory, 512GB SSD & 8x DVD Writer Double Layer Drive (DVD RW)\n802.11ac Wi Fi, Gigabit Ethernet LAN & Bluetooth 4.2LE\n2 Built in 2W Stereo Speakers| Built in 2.0MP Full HD (1080P) Webcam, Wireless Keyboard and Mouse, Windows 10 Home',
+    longDescription: '',
+    stock: 21,
+    media: 7,
+    primaryMedia: 0,
+    isAvailable: true,
+    categoryName: 'Desktops',
+    vendorName: 'Acer'
+  }
+]
