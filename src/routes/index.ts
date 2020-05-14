87a51b86aa49d25121332bb2434345b11c6b1303
Sergei Ovchinnikov <svo393@gmail.com>
2020-05-14 12:14:13 +0300

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express'
 import 'express-async-errors'
-/* eslint-disable */
 import categoriesRouter from './categories'
 import itemsRouter from './items'
 import usersRouter from './users'
 import vendorsRouter from './vendors'
-/* eslint-enable */
 
 export default Router()
   .use('/categories', categoriesRouter)
